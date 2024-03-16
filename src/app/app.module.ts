fb4e291fc4ea1a14beeca7ce17d7a71e1e4493f5
MarioN1010 <mario.navacerrada.arenas10@gmail.com>
2024-03-16 13:09:11 +0100

    sin completar register2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -13,7 +12,7 @@ import { AssignTokenInterceptor } from './modules/auth/interceptors/assign-token
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent,
+    
   ],
   imports: [
     BrowserModule,
