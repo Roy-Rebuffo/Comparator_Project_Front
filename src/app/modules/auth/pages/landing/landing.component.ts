ed9baa5088b3be0e20d333502c97d04bae053e60
eherranzmate <eherranzmate@gmail.com>
2024-03-18 11:33:28 +0100

    feat fix register

--- src/app/modules/auth/pages/landing/landing.component.ts
+++ src/app/modules/auth/pages/landing/landing.component.ts
@@ -8,16 +8,22 @@ import { Router } from '@angular/router';
   styleUrls: ['./landing.component.scss']
 })
 export class LandingComponent {
-  email: string = ""
-  password: string = ""
+  name: string = "";
+  surname: string = "";
+  email: string = "";
+  password: string = "";
+  message: string = "";
+  status: 'loading' | 'unauthorized' | 'success' | 'disabled' | 'error' = 'disabled';
   isSignUpMode: boolean = false;
   isSignUpMode2: boolean = false;
-  constructor(private authService: AuthService, private router: Router){}
+
+  constructor(private authService: AuthService, private router: Router) {}
 
   toggleSignIn() {
     this.isSignUpMode = false;
     this.isSignUpMode2 = false;
   }
+
   toggleSignUp() {
     this.isSignUpMode = true;
     this.isSignUpMode2 = true;
@@ -29,19 +35,46 @@ export class LandingComponent {
     const userObject = {
       email: this.email,
       password: this.password
-    }
+    };
+
+    if (regExpEmail.test(this.email) && regExpPassword.test(this.password)) {
       this.authService.login(userObject).subscribe({
         next: (response: any) => {
-            if(response){
-              sessionStorage.setItem('token-app', JSON.stringify(response.token))
-                this.router.navigate([''])
-            }
-
+          if (response) {
+            sessionStorage.setItem('token-app', JSON.stringify(response.token));
+            this.router.navigate(['']);
+          }
         },
         error: (error) => {
-          console.log(error)
+          console.log(error);
         }
-      })
+      });
+    } else {
+      this.status = 'error';
+      this.message = 'El formulario es inválido, recuerda que la contraseña debe tener de 8 a 12 caracteres y debe incluir al menos un caracter en mayúscula, uno en minúscula, un número y un carácter especial';
     }
-    
+  }
+
+  handleRegister() {
+    this.status = 'loading';
+    const objectToSend = {
+      name: this.name,
+      surname: this.surname,
+      email: this.email,
+      password: this.password
+    };
+
+    this.authService.register(objectToSend).subscribe({
+      next: (response: any) => {
+        this.status = 'success';
+        this.message = response.msg;
+        this.router.navigate(['']);
+      },
+      error: (error) => {
+        console.log(error);
+        this.status = 'unauthorized';
+        this.message = error.error.msg;
+      }
+    });
+  }
 }
