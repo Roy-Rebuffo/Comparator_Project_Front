dc8f579ff7c1e8769b32845a82769c6753e1e291
Roy Rebuffo <roy.rebuffo@bootcamp-upgrade.com>
2024-03-24 16:15:18 +0100

    feat favorites works on search-results

--- src/app/modules/auth/components/favoritos/favoritos.component.ts
+++ src/app/modules/auth/components/favoritos/favoritos.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-favoritos',
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./favoritos.component.scss']
 })
 export class FavoritosComponent {
+  @Input() favoriteProduct: any;
+  favoritos: any = JSON.parse(localStorage.getItem('favoritos')!)
 
+  constructor(){
+    console.log(this.favoritos);
+
+  }
+
+  agregarFavorito(favoriteProduct: any) {
+    this.favoritos.push(favoriteProduct);
+    if (localStorage.getItem('favoritos')) {   localStorage.setItem('favoritos', JSON.stringify([])); }
+  }
 }
