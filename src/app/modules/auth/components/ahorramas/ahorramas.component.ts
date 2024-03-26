70528fbafcee7bfcac5e646769194070da3d3b97
eherranzmate <eherranzmate@gmail.com>
2024-03-26 10:37:12 +0100

    feat fav y botones supermercados + mensajes register + styles

--- src/app/modules/auth/components/ahorramas/ahorramas.component.ts
+++ src/app/modules/auth/components/ahorramas/ahorramas.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ProductService } from '../../services/product.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-ahorramas',
@@ -9,11 +10,41 @@ import { ProductService } from '../../services/product.service';
 export class AhorramasComponent {
   resultados: any[] = [];
 
-  constructor(private productService: ProductService) { }
+  constructor(private router: Router, private productService: ProductService) { }
 
   ngOnInit(): void {
     this.productService.obtenerAhorramas().subscribe((data: any) => {
       this.resultados = data;
     });
   }
+  compareProduct(title: string): void {
+    // Redirigir a la ruta /comparator y pasar el título del producto como parámetro
+    this.router.navigate(['/comparator'], { queryParams: { title: title } })
+  }
+
+  addToFavorites(title: string, image: string, price:number) {
+    const favoritesFromLocal = JSON.parse(localStorage.getItem('favoritos') || '[]');
+
+    console.log(favoritesFromLocal);
+
+    const existingTitleIndex = favoritesFromLocal.findIndex((prodFav: any) => prodFav.title === title && prodFav.image === image && prodFav.price === price);
+
+    if (existingTitleIndex === -1) {
+        // El producto no está en favoritos, así que lo agregamos
+        favoritesFromLocal.push({ title: title, price: price,image: image }); // Agregamos el producto como un objeto con su título
+    } else {
+        // El producto ya está en favoritos, así que lo eliminamos
+        favoritesFromLocal.splice(existingTitleIndex, 1);
+    }
+
+    // Actualizamos la lista de favoritos en el almacenamiento local
+    localStorage.setItem('favoritos', JSON.stringify(favoritesFromLocal));
+
+}
+
+isFavorite(productTitle: string): boolean {
+  const favoritesFromLocal = JSON.parse(localStorage.getItem('favoritos')!);
+  return favoritesFromLocal.some((prodFav: any) => prodFav.title === productTitle);
+}
+
 }
