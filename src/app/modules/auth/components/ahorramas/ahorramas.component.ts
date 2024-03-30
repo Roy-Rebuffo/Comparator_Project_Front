fd661d7533a261ff7bede1fc7b585844afe7fddf
MarioN1010 <mario.navacerrada.arenas10@gmail.com>
2024-03-30 11:29:44 +0100

    filtros rehacer

--- src/app/modules/auth/components/ahorramas/ahorramas.component.ts
+++ src/app/modules/auth/components/ahorramas/ahorramas.component.ts
@@ -1,3 +1,4 @@
+import { SearchResultsComponent } from './../search-results/search-results.component';
 import { Component } from '@angular/core';
 import { ProductService } from '../../services/product.service';
 import { Router } from '@angular/router';
@@ -9,12 +10,15 @@ import { Router } from '@angular/router';
 })
 export class AhorramasComponent {
   resultados: any[] = [];
+  filteredProducts: any[] = [];
 
   constructor(private router: Router, private productService: ProductService) { }
 
   ngOnInit(): void {
     this.productService.obtenerAhorramas().subscribe((data: any) => {
       this.resultados = data;
+      this.filteredProducts = [...this.resultados];
+      console.log(this.resultados);
     });
   }
   compareProduct(title: string): void {
@@ -43,12 +47,94 @@ export class AhorramasComponent {
 }
 
 isFavorite(productTitle: string): boolean {
-  const favoritesFromLocal = JSON.parse(localStorage.getItem('favoritos') || '[]');
-  if (favoritesFromLocal !== null) {
-    return favoritesFromLocal.some((prodFav: any) => prodFav.title === productTitle);
-  } else {
-    return false; // Si no hay favoritos, retornamos falso
+  const favoritesFromLocal = JSON.parse(localStorage.getItem('favoritos')!);
+  return favoritesFromLocal ? favoritesFromLocal.some((prodFav: any) => prodFav.title === productTitle) : false;
+}
+
+checkbox0to2 = false;
+checkbox2to5 = false;
+checkbox5to10 = false;
+checkbox10to15 = false;
+checkbox15to20 = false; 
+checkboxMas20 = false;
+checkboxRedBull =  false;
+checkboxSchweppes=  false;
+checkboxPowerade = false;
+checkboxNestea = false;
+checkboxAquarius = false;
+checkboxCocaCola = false;
+checkboxFanta = false;
+checkboxMonster = false;
+checkboxBurn = false;
+checkboxSobre = false;
+checkboxBotella = false;
+checkboxLata = false;
+checkboxZero = false;
+checkboxLight = false;
+checkboxTonica = false;
+checkboxGaseosa = false;
+checkboxIsotonico = false;
+checkboxCola = false;
+checkboxEnergeticas = false;
+
+
+
+
+filterProducts(min: number, max: number, event: any): void {
+  // Inicializar filteredProducts con todos los productos
+  this.filteredProducts = [...this.resultados];
+
+  // Aplicar filtro de categoría si las casillas de verificación correspondientes están marcadas
+  if (this.checkboxEnergeticas || this.checkboxCola || this.checkboxZero || this.checkboxLight || this.checkboxTonica || this.checkboxGaseosa || this.checkboxIsotonico) {
+    this.filteredProducts = this.resultados.filter(product => {
+      let inCategory = false;
+      if (product.title) {
+        inCategory = (this.checkboxEnergeticas && product.title.includes('Energéticas')) || (this.checkboxCola && product.title.includes('Cola')) || (this.checkboxZero && product.title.includes('Zero')) || (this.checkboxLight && product.title.includes('Light')) || (this.checkboxTonica && product.title.includes('Tónica')) || (this.checkboxGaseosa && product.title.includes('Gaseosa')) || (this.checkboxIsotonico && product.title.includes('Isotónico'));
+      }
+      return inCategory;
+    });
   }
+
+  // Aplicar filtro de precio si las casillas de verificación correspondientes están marcadas
+  if (this.checkbox0to2 || this.checkbox2to5 || this.checkbox5to10 || this.checkbox10to15 || this.checkbox15to20 || this.checkboxMas20) {
+    this.filteredProducts = this.filteredProducts.filter(product => {
+      const price = parseFloat(product.price.replace(',', '.').replace('€', ''));
+      const inRange = (this.checkbox0to2 && price >= 0 && price <= 2) || (this.checkbox2to5 && price >= 2 && price <= 5) || (this.checkbox5to10 && price >= 5 && price <= 10) || (this.checkbox10to15 && price >= 10 && price <= 15) || (this.checkbox15to20 && price >= 15 && price <= 20) || (this.checkboxMas20 && price > 20);
+      return inRange;
+    });
+  }
+}
+
+
+resetFilters(): void {
+this.filteredProducts = [...this.resultados];
+this.checkbox0to2 = false;
+this.checkbox2to5 = false;
+this.checkbox5to10 = false;
+this.checkbox10to15 = false;
+this.checkbox15to20 = false; 
+this.checkboxMas20 = false;
+this.checkboxRedBull =  false;
+this.checkboxSchweppes=  false;
+this.checkboxPowerade = false;
+this.checkboxNestea = false;
+this.checkboxAquarius = false;
+this.checkboxCocaCola = false;
+this.checkboxFanta = false;
+this.checkboxMonster = false;
+this.checkboxBurn = false;
+this.checkboxSobre = false;
+this.checkboxBotella = false;
+this.checkboxLata = false;
+this.checkboxZero = false;
+this.checkboxLight = false;
+this.checkboxTonica = false;
+this.checkboxGaseosa = false;
+this.checkboxIsotonico = false;
+this.checkboxCola = false;
+this.checkboxEnergeticas = false;
+
+// Haz lo mismo para todas las demás casillas de verificación
 }
 
 }
